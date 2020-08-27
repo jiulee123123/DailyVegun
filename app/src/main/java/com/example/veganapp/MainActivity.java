18df04c3923113bd16e472b4c3f7816496a2a9a9
Irene Lee <49191920+jiulee123123@users.noreply.github.com>
2020-08-28 00:30:56 +0900

    Add ListView
    - Add ListView
    - Edit StatusBar Color
    - Delete ActionBar

--- app/src/main/java/com/example/veganapp/MainActivity.java
+++ app/src/main/java/com/example/veganapp/MainActivity.java
@@ -4,32 +4,92 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+
+    ListView listView;
+
+    int images[] = {R.drawable.ic_audio, R.drawable.ic_sun, R.drawable.ic_android};
+    String names[] = {"audio", "sun", "android"};
+    String desc[] = {"This is apple", "This is sun", "This is android"};
+
+    List<ItemsModel> listItems = new ArrayList<>();
+
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<ExampleItem> exampleList = new ArrayList<>();
-        exampleList.add(new ExampleItem(R.drawable.ic_android, "Line 1", "Line 2"));
-        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Line 3", "Line 4"));
-        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Line 5", "Line 6"));
+        listView = findViewById(R.id.listview);
 
-        mRecyclerView = findViewById(R.id.recyclerview);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ExampleAdapter(exampleList);
+        for(int i = 0; i < names.length; i++){
+            ItemsModel itemsModel = new ItemsModel(names[i], desc[i], images[i]);
 
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+            listItems.add(itemsModel);
+        }
 
+        customAdapter = new CustomAdapter(listItems, this );
+
+        listView.setAdapter(customAdapter);
+
+    }
+
+    public class CustomAdapter extends BaseAdapter{
+
+        private List<ItemsModel> itemsModelList;
+        private List<ItemsModel> itemsModelListFiltered;
+        private Context context;
+
+        public CustomAdapter(List<ItemsModel> itemsModelList, Context context) {
+            this.itemsModelList = itemsModelList;
+            this.itemsModelListFiltered = itemsModelList;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return itemsModelListFiltered.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.row_items, null);
+
+            ImageView imageView = view.findViewById(R.id.imageView);
+            TextView itemName = view.findViewById(R.id.itemName);
+            TextView itemDesc = view.findViewById(R.id.itemDesc);
+
+            imageView.setImageResource(itemsModelListFiltered.get(position).getImage());
+            itemName.setText(itemsModelListFiltered.get(position).getName());
+            itemDesc.setText(itemsModelListFiltered.get(position).getDesc());
+
+
+
+            return view;
+        }
     }
 }
